80fe864a8256456caf832557a97847e4b14f8a8b
Chetan Yadav <chetannikum117@gmail.com>
2026-09-24 23:26:43 +0530

    Time: 1 ms (99.77%), Space: 42.5 MB (78.52%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,20 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
+
         int start = 2, end = x / 2;
 
-        if (x == 0 || x == 1)
+        if (x < 2)
             return x;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (mid == x / mid) {
-                return mid;
-            } else if (mid < x / mid) {
+            if (mid <= x / mid) {
                 start = mid + 1;
-            } else {
+
+            } else
                 end = mid - 1;
-            }
         }
-        return start-1;
+        return end;
     }
 }
